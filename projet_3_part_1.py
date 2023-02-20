65c5f4f41b574f945edc8c486ace61a76e6e8990
amelie-ca <116021884+amelie-ca@users.noreply.github.com>
2023-02-20 14:21:12 +0000

    Add map plot

--- projet_3_part_1.py
+++ projet_3_part_1.py
@@ -28,8 +28,24 @@ def AmerCreation (nbamer : int, distX : int, distY : int, xA0 : int, yA0 : int) 
     print("--- Carte cree ---")
     return amers
 
+def MapPlot (etat : np.ndarray):
+    fig, ax = plt.subplots()
+    N = etat.shape[0]
+    x = np.ndarray((int(N/2),))
+    y = np.ndarray((int(N/2),))
+    for i in range(0, N, 2):
+        x[int(i/2)] = etat[i]
+        y[int(i/2)] = etat[i+1]
+
+    ax.scatter(x, y, marker="+")
+    ax.set(xlim=(-1, 10), xticks=np.arange(-1, 11), ylim=(-1, 5), yticks=np.arange(-1, 6))
+
+    ax.set_title('Carte avec les amers')
+    plt.show()
+
 if __name__ == '__main__':
     nbamer = 8
     distX = distY = 2
     xA0, yA0 = (1,2)
     amers = AmerCreation(nbamer, distX, distY, xA0, yA0)
+    MapPlot(amers)
