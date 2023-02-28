7654a963049cb99f37dc9fb045163c0117564dfd
amelie-ca <116021884+amelie-ca@users.noreply.github.com>
2023-02-28 13:06:33 +0100

    Add landmark creation to part 2

--- sources/projet_3_part_2.py
+++ sources/projet_3_part_2.py
@@ -2,3 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from affichage import PlotRes
+
+def AmerCreation(nbamer : int, distX : int, distY : int, xA0 : int, yA0 : int) -> np.ndarray :
+    print("... Creation de la Carte ...")
+    amers = np.empty((2*nbamer,))
+    amers[0:2] = (xA0, yA0)
+    for i in range(2, nbamer, 2) :
+        amers[i] = amers[i-2]+distX
+        amers[i+1] = yA0
+    for i in range(nbamer, 2*nbamer, 2) :
+        amers[i] = amers[i-nbamer]
+        amers[i+1] = yA0+distY
+    return amers
+
