a1e854de0da230819f711bcc13350d792ce3e68b
amelie-ca <116021884+amelie-ca@users.noreply.github.com>
2023-03-01 16:50:27 +0100

    Modification on dynamic noise

--- sources/projet_3_part_2.py
+++ sources/projet_3_part_2.py
@@ -24,7 +24,9 @@ def GenerateRobotPosition(xR0 : float, yR0 : float, pasTau : float, covPos : flo
     U = np.empty((2,N))
     RobPos = np.empty((3,N+1))
     sigma0 = np.diag([covPos0, covPos0, covAng0])
-    sigma = np.diag([covPos, covPos, covAng])
+    sigma1 = np.diag([covPos, covPos, 0.00000000000000000000001])
+    sigma2 = np.diag([0.2*covPos, 0.2*covPos, covAng])
+    sigma3 = sigma1
     RobPos[:,0] = [xR0, yR0, 0] + np.linalg.cholesky(sigma0)@np.random.normal(size=(3,))
     
     #Vecteur de commande 
@@ -36,10 +38,16 @@ def GenerateRobotPosition(xR0 : float, yR0 : float, pasTau : float, covPos : flo
         U[:,k] = [pasTau, 0]
     
     #Calcul de la trajectoire du robot 
-    for k in range(N) :
+    for k in range(N1) :
         RobPos[:,k+1] = [RobPos[0, k]+math.cos(RobPos[2,k])*U[0, k], RobPos[1, k]+math.sin(RobPos[2,k])*U[0, k], RobPos[2,k]+U[1,k]]
-        RobPos[:,k+1] = RobPos[:,k+1] + np.linalg.cholesky(sigma)@np.random.normal(size=(3,))
-    return U, RobPos, N+1, sigma0, sigma
+        RobPos[:,k+1] = RobPos[:,k+1] + np.linalg.cholesky(sigma1)@np.random.normal(size=(3,))
+    for k in range(N1, N1+N2) :
+        RobPos[:,k+1] = [RobPos[0, k]+math.cos(RobPos[2,k])*U[0, k], RobPos[1, k]+math.sin(RobPos[2,k])*U[0, k], RobPos[2,k]+U[1,k]]
+        RobPos[:,k+1] = RobPos[:,k+1] + np.linalg.cholesky(sigma2)@np.random.normal(size=(3,))
+    for k in range(N2+N1, N) :
+        RobPos[:,k+1] = [RobPos[0, k]+math.cos(RobPos[2,k])*U[0, k], RobPos[1, k]+math.sin(RobPos[2,k])*U[0, k], RobPos[2,k]+U[1,k]]
+        RobPos[:,k+1] = RobPos[:,k+1] + np.linalg.cholesky(sigma3)@np.random.normal(size=(3,))
+    return U, RobPos, N1, N2, N3, sigma0, sigma1, sigma2, sigma3
 
 #Pour les mesures : dictlist = [[dict() for x in range(n)] for y in range(6)] permet de creer un dictionnaire 2D, le faire pour les X amers et à chaque amer mettre mesure en angle, distance et cov de bruit associee a chaque elem
 
@@ -52,14 +60,14 @@ if __name__ == '__main__':
     tau = 0.1
     xR0, yR0 = (0,0)
     covDis = 0.0001
-    covAng = np.pi/1000000
+    covAng = np.pi/9500
     covDis0 = 0.000001
     covAng0 = np.pi/10000
 
 
 
     amers = AmerCreation(Nbamer, distX, distY, xA0, yA0)
-    U, Xreel, Nb, Px, PX0 = GenerateRobotPosition(xR0, yR0, tau, covDis, covAng, covDis0, covAng0)
+    U, Xreel, Nb1, Nb2, Nb3, PX0, PX1, PX2, PX3 = GenerateRobotPosition(xR0, yR0, tau, covDis, covAng, covDis0, covAng0)
     PlotRobotMap(Xreel, amers, 'test', (1,1,1))
     print("\n--- Fermez la figure pour terminer ---\n")
     plt.show()
