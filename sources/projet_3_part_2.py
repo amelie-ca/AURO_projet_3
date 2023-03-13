498b10a8a8f099ee2515f633fabe45df495770e3
amelie-ca <116021884+amelie-ca@users.noreply.github.com>
2023-03-13 15:15:19 +0100

    Kalman time update
    Add Kalman time update but it does not work

--- sources/projet_3_part_2.py
+++ sources/projet_3_part_2.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 from affichage import PlotRes, PlotRobotMap
 
-def AmerCreation(nbamer : int, distX : float, distY : float, xA0 : float, yA0 : float) -> np.ndarray :
+def AmerCreation(nbamer : int, distX : float, distY : float, xA0 : float, yA0 : float, dispA : float) -> np.ndarray :
     print("... Creation de la Carte ...")
     amers = np.empty((2*nbamer,))
     amers[0:2] = (xA0, yA0)
@@ -13,7 +13,9 @@ def AmerCreation(nbamer : int, distX : float, distY : float, xA0 : float, yA0 : 
     for i in range(nbamer, 2*nbamer, 2) :
         amers[i] = amers[i-nbamer]
         amers[i+1] = yA0+distY
-    return amers
+    sigma = np.diag(dispA*np.ones(2*nbamer))
+    amersB = amers + np.linalg.cholesky(sigma)@np.random.normal(size=(2*nbamer,))
+    return amers, amersB
 
 def GenerateRobotPosition(xR0 : float, yR0 : float, pasTau : float, covPos : float, covAng : float, covPos0 : float, covAng0 : float ) -> np.ndarray :
     N1 = 70
@@ -73,6 +75,39 @@ def JacobF(X : np.ndarray, U : np.ndarray) :
     F[2,2] = 1
     return F
 
+def MeasShortage(Z, X, covD, covA) :
+    Mes = False 
+    Zuse = []
+    indZ = []
+    Zest = []
+    for Zi in Z :
+        if not np.isnan(Zi["range"]) :
+            Mes = True 
+            Zuse.append(Zi["range"])
+            Zuse.append(Zi["bearing"])
+            indZ.append(Zi["amer"])
+            Zest.append(math.sqrt((X[int(3+Zi["amer"])]-X[0])**2+(X[int(3+Zi["amer"]+1)]-X[1])**2))
+            Zest.append(math.atan2(X[int(3+Zi["amer"]+1)]-X[1] ,X[int(3+Zi["amer"])]-X[0]))
+            
+    Zuse = np.array(Zuse)
+    Zest = np.array(Zest)
+    indZ = np.array(indZ)
+    H = np.zeros((Zuse.shape[0], X.shape[0]))
+    for i in range(indZ.shape[0]) :
+        H[int(2*i)][0] = -2*(X[int(3+indZ[i])]-X[0])*1/(2*math.sqrt(Zuse[int(2*i)]))
+        H[int(2*i)][1] = -2*(X[int(3+indZ[i]+1)]-X[1])*1/(2*math.sqrt(Zuse[int(2*i)]))
+        H[2*i][int(3+indZ[i])] = 2*(X[int(3+indZ[i])]-X[0])*1/(2*math.sqrt(Zuse[int(2*i)]))
+        H[2*i][int(3+indZ[i]+1)] = 2*(X[int(3+indZ[i]+1)]-X[1])*1/(2*math.sqrt(Zuse[int(2*i)]))
+        H[int(2*i+1)][0] = (X[int(3+indZ[i]+1)]-X[2])/((X[int(3+indZ[i])]-X[1])**2+(X[int(3+indZ[i]+1)]-X[2])**2)
+        H[int(2*i+1)][1] = -(X[int(3+indZ[i])]-X[1])/((X[int(3+indZ[i])]-X[1])**2+(X[int(3+indZ[i]+1)]-X[2])**2)
+        H[2*i+1][3] = -1 
+        H[2*i+1][int(3+indZ[i])] = -(X[int(3+indZ[i]+1)]-X[2])/((X[int(3+indZ[i])]-X[1])**2+(X[int(3+indZ[i]+1)]-X[2])**2)
+        H[2*i+1][int(3+indZ[i]+1)] = (X[int(3+indZ[i])]-X[1])/((X[int(3+indZ[i])]-X[1])**2+(X[int(3+indZ[i]+1)]-X[2])**2) 
+    sigma = np.zeros((Zuse.shape[0],Zuse.shape[0]))
+    for i in range(int(Zuse.shape[0]/2)) :
+        sigma[2*i][2*i] = covD
+        sigma[2*i+1][2*i+1] = covA
+    return Zuse, Zest,  H, sigma, Mes
 
 #Pour le tri des mesures, faire une fonction, on parcours la liste des amers et si un amer ne fait pas partie de la liste, on l'ajoute. 
 if __name__ == '__main__':
@@ -91,10 +126,10 @@ if __name__ == '__main__':
     covDisMes = 0.01
     covAngMes = np.pi/20
     
-    amers = AmerCreation(Nbamer, distX, distY, xA0, yA0)
+    Mamer, amers = AmerCreation(Nbamer, distX, distY, xA0, yA0, dispAmers)
     U, Xreel, Nb1, Nb2, Nb3, PX0, Qw1, Qw2, Qw3 = GenerateRobotPosition(xR0, yR0, tau, covDis, covAng, covDis0, covAng0)
     N = Nb1+Nb2+Nb3+1
-    Z = GenerateRobotMeasurment (N, Nbamer, Xreel, amers, covAng, covDis)
+    Zr = GenerateRobotMeasurment (N, Nbamer, Xreel, amers, covAng, covDis)
     PlotRobotMap(Xreel, amers, 'test', (1,1,1))
     print("\n--- Fermez la figure pour terminer ---\n")
     plt.show()
@@ -107,7 +142,8 @@ if __name__ == '__main__':
     Xpred = np.empty((3+Nbamer*2, N))
     Ppred = np.empty((3+Nbamer*2,3+Nbamer*2,N))
     
-    Xest[:,0] = np.append([xR0, yR0, 0], amers)
+    Xest[:,0] = np.append([xR0, yR0, 0], Mamer)
+
     Pest[:,:,0] = np.diag(np.append([0, 0, 0], dispAmers*np.ones(2*Nbamer)))
 
     #Boucle de filtrage 
@@ -117,8 +153,9 @@ if __name__ == '__main__':
         Xpred[0, k] = Xest[0,k-1]+U[0,k-1]*math.cos(Xest[2,k-1]) 
         Xpred[1, k] = Xest[1,k-1]+U[0,k-1]*math.sin(Xest[2,k-1]) 
         Xpred[2, k] = Xest[2,k-1]+U[1,k-1]
+        Xpred[3:, k] = Xest[3:, k-1]
         F = JacobF(Xest[:,k-1], U[:,k-1])
-        Qw = np.zeros((3+2*Nbamer,3+2*Nbamer))
+        Qw = np.diag(0.000001*np.ones(3+2*Nbamer))
         if k<Nb1 :
             Qw[:3,:3] = Qw1
         elif k<Nb2+Nb1 :
@@ -127,7 +164,17 @@ if __name__ == '__main__':
             Qw[:3,:3] = Qw3
         Ppred[:,:,k] = F@Pest[:,:,k-1]@F.T + Qw
 
-        #Mise a jour
-        Xest[:,k] = Xpred[:,k]
-        Pest[:,:,k] = Ppred[:,:,k]
+        Z, Zest, H, Rv, Mes = MeasShortage(Zr[k], Xpred[:,k], covDisMes, covAngMes)
+        #Mise a jour 
+        if Mes :
+            S = Rv+H@Ppred[:,:,k]@H.T
+            K = Ppred[:,:,k]@H.T@np.linalg.inv(S)
+            Xest[:,k] = Xpred[:,k] + K@(Z-Zest)
+            Pest[:,:,k] = Ppred[:,:,k] - K@H@Ppred[:,:,k]
+            
+        else :
+            Xest[:,k] = Xpred[:,k]
+            Pest[:,:,k] = Ppred[:,:,k]
+    
     print("Filtrage - Terminé")
+    PlotRes(Xreel, amers, Xest, Pest, N)
